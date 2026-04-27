14f7c4be30656e90748178e59096169c2a7e9507
Lucas Celis <celis.lucase@gmail.com>
2026-04-27 19:58:50 -0300

    Create DQL.sql

---
+++ codigos/DQL.sql
@@ -0,0 +1,19 @@
+/* DQL - Data Query Language */
+
+/* É possível selecionar colunas específicas ou utlizar o '*' para todas as colunas */
+
+/* Estrutura com colunas */
+
+SELECT coluna1, coluna2 FROM nome_tabela;
+
+/* Estrutura com '*' */
+
+SELECT * FROM nome_tabela;
+
+/* Exemplo com colunas */
+
+SELECT NOME, CPF, ENDERECO FROM CLIENTE;
+
+/* Exemplo com '*' */
+
+SELECT * FROM CLIENTE;
